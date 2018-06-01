595c93b02657590f50ad5c57e315e20ef845e92c
Carlo Cassinari <carlo@techmakers.io>
2018-06-01 11:47:16 +0200

    u

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,3 @@
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See dashboard-tests.js for an example of importing.
 export const tmdashboard = {} ;
 
 tmdashboard.test = true ;
