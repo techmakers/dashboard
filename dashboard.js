533736e8a45c981bab4203a59a64c3b861a95c8a
Carlo Cassinari <carlo@techmakers.io>
2018-06-01 11:51:30 +0200

    u

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,14 @@ export const tmdashboard = {} ;
 
 tmdashboard.test = true ;
 
+tmdashboard.init = function(options){
+	if (options.collection){
+		tmdashboard.registeredDashboardsCollection = options.collection ;
+	} else {
+		tmdashboard.registeredDashboardsCollection = new Mongo.Collection('tmdashboard.dashboards') ;
+	}
+}
+
 tmdashboard.register = function(
 	name,
 	route,
