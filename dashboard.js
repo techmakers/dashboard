476f882ecefbf4fd7c5967147d25b72662700f31
Carlo Cassinari <carlo@techmakers.io>
2018-06-01 11:43:06 +0200

    update

--- dashboard.js
+++ dashboard.js
@@ -5,3 +5,20 @@
 export const tmdashboard = {} ;
 
 tmdashboard.test = true ;
+
+tmdashboard.register = function(
+	name,
+	route,
+	description,
+	roles,
+	index
+){
+
+};
+
+tmdashboard.notify = function(
+	name,
+	message
+){
+
+};
--- package.js
+++ package.js
@@ -13,7 +13,14 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.6.1.1');
   api.use('ecmascript');
+
+  api.addFile('dashboard-collections.js') ;
+  api.addFile('dashboard-publications.js','server') ;
+  api.addFile('dashboard-server.js', 'server');
+  api.addFile('dashboard.html','client');
+  api.addFile('dashboard-client.js', 'client');
   api.mainModule('dashboard.js');
+
 });
 
 Package.onTest(function(api) {
